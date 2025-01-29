e54072665835de8e18978f6d648f6f760d7ec011
Robert <rdshakura@gmail.com>
2025-01-29 19:00:21 +0300

    fix promise

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
   const cookieStore = await cookies();
   const theme = cookieStore.get("theme")?.value || "light";
